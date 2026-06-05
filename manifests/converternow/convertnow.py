5568c4ce8cc1c556db7641472f0b5cffbf663229
aegis-fate-gh <stephen.hudgins@gmail.com>
2026-06-04 21:35:13 -0500

    image format swap

--- manifests/converternow/convertnow.py
+++ manifests/converternow/convertnow.py
@@ -4,7 +4,7 @@ from diagrams.onprem import network
 
 with Diagram("Converternow", show=False, direction="TB"):
     with Cluster("jovian-prod"):
-        converternow = Custom("Converter Now", "./local_icons/converternow.jpg")
+        converternow = Custom("Converter Now", "./local_icons/converternow.png")
     with Cluster("kube-system"):
         traefik = network.Traefik("Traefik\nInternal Proxy")
 
