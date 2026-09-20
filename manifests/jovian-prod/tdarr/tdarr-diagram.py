e45580a063bcb847ba6e482d10d44bc6469a4e21
aegis-fate-gh <stephen.hudgins@gmail.com>
2026-09-20 00:15:03 -0500

    feat: doc updates for tdarr

--- manifests/jovian-prod/tdarr/tdarr-diagram.py
+++ manifests/jovian-prod/tdarr/tdarr-diagram.py
@@ -2,16 +2,19 @@ from diagrams import Diagram, Cluster, Edge
 from diagrams.custom import Custom
 from diagrams.onprem import network, storage
 
-with Diagram("APP", show=False, direction="TB"):
+with Diagram("Tdarr", show=False, direction="TB"):
     with Cluster("Eos"):
+        tdarr_lxc = Custom("Tdarr Workers", "/app/icons/tdarr.png")
         with Cluster("k3s"):
             with Cluster("Namespace: jovian-prod"):
-                tdarr = Custom("Tdarr", "/app/icons/tdarr.png")
+                tdarr = Custom("Tdarr Server", "/app/icons/tdarr.png")
                 ceph = storage.Ceph("CephFS PVC")
                 smb = Custom("SMB to UNAS-Pro", "/app/icons/unifi-drive.png")
+                loadbalancer = Custom("MetalLB IP", "/app/icons/metallb.png")
             with Cluster("Namespace: kube-system"):
                 traefik = network.Traefik("Traefik\nInternal Proxy")
 
     traefik >> Edge(color="blue", style="dotted") >> tdarr
     ceph >> Edge(color="black", style="solid") >> tdarr
     smb >> Edge(color="black", style="solid") >> tdarr
+    tdarr_lxc >> Edge(color="blue", style="dotted") >> loadbalancer >> Edge(color="blue", style="dotted") >> tdarr
