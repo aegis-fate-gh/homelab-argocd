d83696a1a8e3900c08e336b3a471cee66a1d5804
aegis-fate-gh <stephen.hudgins@gmail.com>
2026-09-20 11:23:47 -0500

    feat: diagram for ytdl

---
+++ manifests/jovian-prod/ytdl/app-diagram.py
@@ -0,0 +1,23 @@
+from diagrams import Diagram, Cluster, Edge
+from diagrams.custom import Custom
+from diagrams.onprem import network, storage
+from diagrams.k8s.storage import PVC
+from diagrams.onprem.database import Mongodb
+
+with Diagram("app", show=False, direction="TB"):
+    with Cluster("Homelab"):
+        unas_pro = Custom("UNAS-Pro", "/app/icons/unifi-drive.png")
+        with Cluster("Eos"):
+            with Cluster("k3s"):
+                with Cluster("Namespace: jovian-prod"):
+                    ytdl = Custom("YouTube DL Material", "/app/icons/youtube-dl.png")
+                    ceph = storage.Ceph("CephFS PVC")
+                    smb = PVC("Media SMB PVC")
+                    mongo = Mongodb("YTDL Mongo")
+                with Cluster("Namespace: kube-system"):
+                    traefik = network.Traefik("Traefik\nInternal Proxy")
+
+    traefik >> Edge(color="blue", style="dotted") >> ytdl
+    ceph >> Edge(color="darkorange", style="solid") >> ytdl
+    smb - Edge(color="black", style="solid") - ytdl, mongo
+    unas_pro - Edge(color="royalblue", style="solid") - smb
